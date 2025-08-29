9773825f3b2f7446fdaa164949cdd88f67f4c763
ZZX-Laboratories <zzx-laboratories@protonmail.ch>
2025-08-29 15:43:18 -0400

    Create shuffle.js

---
+++ inspiration/figures/modules/shuffle.js
@@ -0,0 +1,16 @@
+export function shuffle(arr) {
+  const a = arr.slice();
+  if (window.crypto?.getRandomValues) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const r = new Uint32Array(1); crypto.getRandomValues(r);
+      const j = r[0] % (i + 1);
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+  } else {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.random() * (i + 1) | 0;
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+  }
+  return a;
+}
