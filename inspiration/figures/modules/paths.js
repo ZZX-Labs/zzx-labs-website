bac8b7702d88626b35734a45f8ceffc16d590345
ZZX-Laboratories <zzx-laboratories@protonmail.ch>
2025-08-29 15:41:46 -0400

    Update and rename placeholder to paths.js

---
+++ inspiration/figures/modules/paths.js
@@ -0,0 +1,10 @@
+export const BASE      = new URL('../', import.meta.url);
+export const PALETTE   = new URL('color-palette.json', BASE);
+export const FIGURES   = new URL('figures.json', BASE);
+export const URLS_URL  = new URL('urls.json', BASE);
+export const CARDS_DIR = new URL('cards/', BASE);
+export const IMGS_DIR  = new URL('images/', BASE);
+
+// Wikipedia
+export const MW_API = 'https://en.wikipedia.org/w/api.php';
+export const ORIGIN = '&origin=*';
