930a94f93ca63f8b00c9a6b3656ee9439e33dd7e
ZZX-Laboratories <zzx-laboratories@protonmail.ch>
2025-08-29 15:42:03 -0400

    Create dom.js

---
+++ inspiration/figures/modules/dom.js
@@ -0,0 +1,4 @@
+export const gridEl   = document.getElementById('figure-grid');
+export const tpl      = document.getElementById('tpl-figure-card');
+export const filterEl = document.getElementById('figure-filter');
+export const countEl  = document.getElementById('figure-count');
