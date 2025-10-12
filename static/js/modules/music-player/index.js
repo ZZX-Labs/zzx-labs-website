ef5a4f21f4bbae12c61b4d3a84bf18ea26ad3dcb
ZZX-Laboratories <zzx-laboratories@protonmail.ch>
2025-10-12 18:22:24 -0400

    Update index.js

--- static/js/modules/music-player/index.js
+++ static/js/modules/music-player/index.js
@@ -1,14 +1,14 @@
 // static/js/modules/music-player/index.js
 // Entry: export mount() and auto-mount a single player if [data-mp] exists.
 
-import { mount } from './player.js';
+import { mount } from './mount.js';
 
 export { mount };
 
 // Auto-mount first [data-mp] on the page (back-compat with previous build)
 const autoRoot = document.querySelector('[data-mp]');
 if (autoRoot) {
-  // You can override via data-* attributes on the element. See player.js for options.
+  // You can override via data-* attributes on the element. See mount.js for options.
   mount(autoRoot);
 }
 
