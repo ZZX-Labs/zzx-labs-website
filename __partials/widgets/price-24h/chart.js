99f6f17dad35c6b7c0fbff6b2fb01edd489931b9
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-01-07 04:39:41 -0500

    Update chart.js

--- __partials/widgets/price-24h/chart.js
+++ __partials/widgets/price-24h/chart.js
@@ -1,1 +1,12 @@
+// __partials/widgets/price-24h/chart.js
+// Chart module wrapper for price-24h
 
+(function () {
+  "use strict";
+
+  const NS = (window.ZZXChart = window.ZZXChart || {});
+
+  NS.drawPrice24 = function drawPrice24(canvas, candles, deltaIsUp) {
+    window.ZZXPlotter?.drawHL?.(canvas, candles, deltaIsUp);
+  };
+})();
