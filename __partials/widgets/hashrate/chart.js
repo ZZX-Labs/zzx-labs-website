4062ffd661504bd054a75b797edf3b65b20aa5db
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-01-07 19:32:28 -0500

    Create chart.js

---
+++ __partials/widgets/hashrate/chart.js
@@ -0,0 +1,21 @@
+// __partials/widgets/hashrate/chart.js
+// Wires plotter -> DOM.
+// Exposes: window.ZZXHashrateChart.draw(svgEl, valuesZH)
+
+(function () {
+  "use strict";
+
+  const W = window;
+  const NS = (W.ZZXHashrateChart = W.ZZXHashrateChart || {});
+
+  NS.draw = function draw(svgEl, valuesZH) {
+    if (!svgEl) return;
+    const pLine = svgEl.querySelector("[data-hr-line]");
+    const pArea = svgEl.querySelector("[data-hr-area]");
+    if (!pLine || !pArea) return;
+
+    const res = W.ZZXHashratePlotter?.build ? W.ZZXHashratePlotter.build(valuesZH) : { line:"", area:"" };
+    pLine.setAttribute("d", res.line || "");
+    pArea.setAttribute("d", res.area || "");
+  };
+})();
