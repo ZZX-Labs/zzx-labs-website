f639978c417a368401e908f607cf8509ff608f01
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-05-29 18:05:36 -0400

    Create script.js

---
+++ cyberchef/script.js
@@ -0,0 +1,6 @@
+(function () {
+    const script = document.createElement("script");
+    script.src = "assets/cyberchef-page.js";
+    script.defer = true;
+    document.head.appendChild(script);
+})();
