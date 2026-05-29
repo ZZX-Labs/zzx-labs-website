0609f3bdb0adc4e19e202ae75c0f39a004303796
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-05-29 18:51:46 -0400

    Create script.js

---
+++ cyberchef/app/script.js
@@ -0,0 +1,72 @@
+(function () {
+  function installStorageShim() {
+    function memoryStorage() {
+      var mem = {};
+      return {
+        getItem: function (k) { return Object.prototype.hasOwnProperty.call(mem, k) ? mem[k] : null; },
+        setItem: function (k, v) { mem[k] = String(v); },
+        removeItem: function (k) { delete mem[k]; },
+        clear: function () { mem = {}; },
+        key: function (i) { return Object.keys(mem)[i] || null; },
+        get length() { return Object.keys(mem).length; }
+      };
+    }
+
+    function storageWorks(name) {
+      try {
+        var s = window[name];
+        var k = "__zzx_storage_test__";
+        s.setItem(k, "1");
+        s.removeItem(k);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+
+    try {
+      if (!storageWorks("localStorage")) {
+        Object.defineProperty(window, "localStorage", {
+          configurable: true,
+          value: memoryStorage()
+        });
+      }
+    } catch (e) {}
+
+    try {
+      if (!storageWorks("sessionStorage")) {
+        Object.defineProperty(window, "sessionStorage", {
+          configurable: true,
+          value: memoryStorage()
+        });
+      }
+    } catch (e) {}
+
+    try {
+      var originalSetItem = Storage.prototype.setItem;
+      Storage.prototype.setItem = function (key, value) {
+        try {
+          return originalSetItem.call(this, key, value);
+        } catch (e) {
+          if (
+            e &&
+            (
+              e.name === "QuotaExceededError" ||
+              e.name === "NS_ERROR_DOM_QUOTA_REACHED"
+            )
+          ) {
+            return null;
+          }
+          throw e;
+        }
+      };
+    } catch (e) {}
+  }
+
+  installStorageShim();
+
+  var script = document.createElement("script");
+  script.src = "assets/main.js";
+  script.defer = false;
+  document.head.appendChild(script);
+})();
