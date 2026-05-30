cd24c79f9c9f11766ef9c0b16b756a8a546e78fd
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-05-29 20:29:50 -0400

    Update hook.js

--- cyberchef/hook.js
+++ cyberchef/hook.js
@@ -4,56 +4,51 @@
     window.ZZX = window.ZZX || {};
 
     window.ZZX.CYBERCHEF = {
-
         version: "v11.0.0",
 
         title: "CyberChefZZX",
 
-        localUrl: "./app/",
+        modifiedUrl: "./",
+        nativeUrl: "./app/",
+        upstreamUrl: "https://gchq.github.io/CyberChef/",
 
-        upstreamUrl:
-            "https://gchq.github.io/CyberChef/",
-
-        defaultSource: "local",
+        defaultSource: "modified",
 
         allowSourceSwitching: true,
-
         allowFullscreen: true,
-
         allowPopout: true,
-
         allowReload: true,
-
         allowStatusMessages: true,
+        allowDirectRuntime: true,
 
-        iframeId: "cz-frame",
-
+        runtimeId: "cz-runtime",
         containerId: "cz-container",
-
         statusId: "cz-status",
-
         sourceId: "cz-source",
-
         loadButtonId: "cz-load",
-
         refreshButtonId: "cz-refresh",
-
         activeSourceId: "cz-active-source",
-
         frameStateId: "cz-frame-state",
+        modificationsId: "cz-modifications",
+
+        cyberChefMainScript: "app/assets/main.js",
+        cyberChefMainStylesheet: "app/assets/main.css",
 
         storageKeys: {
+            source: "zzxCyberChefSource",
+            fullscreen: "zzxCyberChefFullscreen",
+            compact: "zzxCyberChefCompact",
+            scale: "zzxCyberChefScale",
+            theme: "zzxCyberChefTheme",
+            cyberTheme: "zzxCyberChefInternalTheme",
+            lastLoaded: "zzxCyberChefLastLoaded"
+        },
 
-            source:
-                "zzxCyberChefSource",
-
-            fullscreen:
-                "zzxCyberChefFullscreen",
-
-            lastLoaded:
-                "zzxCyberChefLastLoaded"
-
+        defaultOptions: {
+            theme: "dark",
+            wordWrap: true,
+            showErrors: true,
+            updateUrl: true
         }
     };
-
 })();
