dec2325fc754c25f64fa905570bb69294ed1490d
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-09-07 18:54:30 -0400

    Update ZZXBitAvgPublisher version to 7

--- __partials/widgets/bitavg/js/publisher.js
+++ __partials/widgets/bitavg/js/publisher.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const W=window;
-  if(W.ZZXBitAvgPublisher?.__version>=5)return;
+  if(W.ZZXBitAvgPublisher?.__version>=7)return;
 
   const CHANNEL="zzx:bpi:update";
   const PROVIDER_ID="bitavg";
@@ -14,12 +14,21 @@
       mode:MODE_ID,
       label:"Global BPI",
       price_usd:Number(model.bpi),
+      weighted_price_usd:Number(model.weightedBpi),
+      unweighted_price_usd:Number(model.unweightedBpi),
+      weights_enabled:!!model.weightsEnabled,
       method:model.method,
+      method_weighted:model.methodWeighted,
+      method_unweighted:model.methodUnweighted,
       exchange_count:model.exchanges.length,
       market_count:model.markets,
+      observed_market_count:model.observedMarkets,
+      quarantined_market_count:model.quarantinedMarkets,
       fiat_count:model.currencies.length,
       volume_24h_btc:Number(model.volume),
       weight_sum:Number(model.weightSum),
+      weight_basis:
+        "eligible market 24h BTC volume / eligible global 24h BTC volume",
       updated_at:model.updatedAt||new Date().toISOString(),
       rendered_at:Date.now(),
       transport:String(transport||""),
@@ -47,7 +56,7 @@
   }
 
   W.ZZXBitAvgPublisher=Object.freeze({
-    __version:5,
+    __version:7,
     channel:CHANNEL,
     providerId:PROVIDER_ID,
     modeId:MODE_ID,
