57d5b568eeed91c96ead58adde81b43f339b66ad
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-09-26 18:18:50 -0400

    Update order value for Ammo category to 16

--- __partials/widgets/bitcoin-ticker/js/purchasing-power/ammo.js
+++ __partials/widgets/bitcoin-ticker/js/purchasing-power/ammo.js
@@ -2,7 +2,7 @@
   "use strict";
   const W=window;
   const spec=W.ZZXBitcoinTickerPurchasingPowerRegistry?.register({
-    id:"ammo",label:"Ammo",order:15
+    id:"ammo",label:"Ammo",order:16
   });
-  W.ZZXBitcoinTickerPurchasingPowerCategoryAmmo=spec||Object.freeze({id:"ammo",label:"Ammo",order:15,panel:"references"});
+  W.ZZXBitcoinTickerPurchasingPowerCategoryAmmo=spec||Object.freeze({id:"ammo",label:"Ammo",order:16,panel:"references"});
 })();
