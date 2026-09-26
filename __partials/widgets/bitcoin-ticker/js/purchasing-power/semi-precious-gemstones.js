6da35f9dd4bad39a3ced38dc36d549b12c8cd7ad
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-09-26 18:19:27 -0400

    Fix label for semi-precious gemstones

--- __partials/widgets/bitcoin-ticker/js/purchasing-power/semi-precious-gemstones.js
+++ __partials/widgets/bitcoin-ticker/js/purchasing-power/semi-precious-gemstones.js
@@ -2,7 +2,7 @@
   "use strict";
   const W=window;
   const spec=W.ZZXBitcoinTickerPurchasingPowerRegistry?.register({
-    id:"semi-precious-gemstones",label:"Semi Precious Gemstones",order:9
+    id:"semi-precious-gemstones",label:"Semi Precious Gem Stones",order:9
   });
-  W.ZZXBitcoinTickerPurchasingPowerCategorySemiPreciousGemstones=spec||Object.freeze({id:"semi-precious-gemstones",label:"Semi Precious Gemstones",order:9,panel:"references"});
+  W.ZZXBitcoinTickerPurchasingPowerCategorySemiPreciousGemstones=spec||Object.freeze({id:"semi-precious-gemstones",label:"Semi Precious Gem Stones",order:9,panel:"references"});
 })();
