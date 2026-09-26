cd2d0f74f2b1f56967b1766349bc78d512370cdc
ZZX-Labs R&D <zzx-laboratories@protonmail.ch>
2026-09-26 18:19:07 -0400

    Update order value for Arms category in Bitcoin ticker

--- __partials/widgets/bitcoin-ticker/js/purchasing-power/arms.js
+++ __partials/widgets/bitcoin-ticker/js/purchasing-power/arms.js
@@ -2,7 +2,7 @@
   "use strict";
   const W=window;
   const spec=W.ZZXBitcoinTickerPurchasingPowerRegistry?.register({
-    id:"arms",label:"Arms",order:14
+    id:"arms",label:"Arms",order:15
   });
-  W.ZZXBitcoinTickerPurchasingPowerCategoryArms=spec||Object.freeze({id:"arms",label:"Arms",order:14,panel:"references"});
+  W.ZZXBitcoinTickerPurchasingPowerCategoryArms=spec||Object.freeze({id:"arms",label:"Arms",order:15,panel:"references"});
 })();
